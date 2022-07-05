0d8931b1e654dfafb2f72cf6fac83dfc21c69601
Sudip <sudippaudel944@gmail.com>
2022-07-05 10:42:11 +0545

    Practise commit

--- List and Tuples/05_tuple_methods.py
+++ List and Tuples/05_tuple_methods.py
@@ -4,4 +4,4 @@ t1=(1,4,2,7,3,9,1,2,5,7)
 print(t1.count(1))   #counts how many times given value 1 is repeated in the above tuple t1.
 print(t1.index(2))    #prints the first index value of 2 even though it is repeated in the tuple.
 
-print(t1.index(9))
+print(t1.index(9))   #prints the index of the element 9 in the tuple.
