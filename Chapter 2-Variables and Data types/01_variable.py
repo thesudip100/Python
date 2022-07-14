c6eed11eace9837b31a278ae136bb5daef5538b0
Sudip <sudippaudel944@gmail.com>
2022-07-15 00:04:38 +0545

    Some data types were added and modified

--- Chapter 2-Variables and Data types/01_variable.py
+++ Chapter 2-Variables and Data types/01_variable.py
@@ -1,4 +1,9 @@
-# VARIABLES AND DATA TYPES IN PYTHON
+'''VARIABLES AND DATA TYPES IN PYTHON 
+
+1) None Data Type
+2) Numeric- int,float,complex, bool
+3) sequence- list, tuple, set,strings, range
+4)Dictionaries/Mapping'''
 
 # 1) STRING DATA TYPE
 a = "Sudip"  #string; can be enclosed either in single, double or triple quote.
@@ -12,15 +17,14 @@ man'''
 
 
 
-# 2) INTEGER DATA TYPE
+# 2) NUMERIC DATA TYPE - consists of int, float, complex and bool
 
 b = 367      #integer
 
-
-
-# 3) FLOAT DATA TYPE
-
 c = 45.32    #floating point numbers
+
+com= 5+ 6j  # complex data type
+print(type(com))   # gives the data type as complex
 
 
 
@@ -37,13 +41,13 @@ print(c)
 
 
 
-#4)BOOLEANS DATA TYPE(returns either true or false)
+#BOOLEANS DATA TYPE(returns either true or false)
 
 d=True
 print(d)
 
 
-#5)NONE DATA TYPES - is used to denote nothing in python
+#3)NONE DATA TYPES - is used to denote nothing in python
 
 e=None;
 print(e);
@@ -55,3 +59,23 @@ print(type(b))  #this will print the datatype of variable b.
 
 
 # IDENTIFIERS are names given to class, function or variables.
+
+# We can convert data type into complex as well.
+
+p=5
+q=6
+print(complex(p,q))  #prints 5+6j
+
+####################### RANGE #########################
+
+# let us print values from 1 to 10.
+ran=range(1,11)  # this has stored values from 1 to 10 in variable a.
+print(type(ran))
+
+# But, how to access these elements from 1 to 10? The better way is using for loop but we have not read for loop as of now, so we will access them using list.
+
+print(list(ran))   # prints the values from 1 to 10 in list format.
+
+# Can we print only even numbers from 1 to 10 using range? Yes,we can.
+
+print(list(range(2,11,2)))   # starting value, end value, skip value is given inside the bracket.
