8e14e55946f3ab8e28479fa7c83d37ae9f8dc73b
Sudip <sudippaudel944@gmail.com>
2022-07-16 22:15:42 +0545

    some changes were made in operators

--- Chapter 2-Variables and Data types/02_operators.py
+++ Chapter 2-Variables and Data types/02_operators.py
@@ -12,9 +12,10 @@ print("Modulo Division is ",a%b)
 p=43
 p+=2
 print(p)
+q,r=6,7      #multiple assignment; q=6 qnd r=7
 
 
-#COMPARISION OPERATORS (==,>,<,>=,<=,!=)   => returns booleans
+#COMPARISION/Relational OPERATORS (==,>,<,>=,<=,!=)   => returns booleans
 print(a==b) 
 print(a>=b)
 print(a<=b)
@@ -26,3 +27,11 @@ bool2=False
 print(bool1 and bool2)
 print(bool1 or bool2)
 print(not bool1)
+
+data1=5
+data2=6
+print((data1<6)and(data2>7))
+
+#Unary operators
+n=7
+n=-n;   #unary negation; -7
