3473973eb68a4fd287c59d04018576f8c15d3966
Sudip <sudippaudel944@gmail.com>
2022-07-16 22:30:55 +0545

    changed topics of chapters and added numbers system

---
+++ 7_number_system.py
@@ -0,0 +1,11 @@
+'''Using python, we can convert one number system into another. For eg- binary to decimal, octal to decimal, decimal to hexadecimal and so on.'''
+
+print(bin(5))  #prints binary number of 5 i.e 0b101. Here, 0b indicates that it is binary and 101 is the binary code of 5.
+print(0b101)   #this will convert binary to decimal
+
+print(oct(43))  #prints 0o53. Here, 0o is used to denote that it is octal conversion.
+print(0o53)   #octal to decimal
+
+
+print(hex(10))  #prints 0xa. Here, 0x is used to denote that it is hexadecimal conversion.
+print(0xa)  #hexadecimal to decimal conversion.
