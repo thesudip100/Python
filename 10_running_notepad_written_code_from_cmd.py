9ff49ee2e16f85ef7e19a12ee952953bc97a2a3c
Sudip <sudippaudel944@gmail.com>
2022-07-17 18:56:05 +0545

    Running code from cmd written in notepad

---
+++ 10_running_notepad_written_code_from_cmd.py
@@ -0,0 +1,12 @@
+'''We can also write our code in notepad and run it from the command prompt without the need of any IDE.
+
+Lets see the steps for it.
+
+1) Open notepad and write your python code in it.
+2) Press Ctrl + S to save it.
+3) Give the file name and at the end of file name, give .py extension to indicate it as python file. eg- hello.py
+4) Save the file in your desired folder.
+5) After saving file, open command prompt and run it as administrator.
+6) Then, you need to type cd and the path of the file which you can find at the top of your file explorer when you click on it. eg. cd Desktop
+7) After you enter in your folder from command prompt, type 'python <file_name>' and hit enter and your code will run from cmd. eg- python hello.py
+'''
